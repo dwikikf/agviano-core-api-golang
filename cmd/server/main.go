2170e18641c1aa6fedfc8868ed99c17274f5433e
dwikikf <dwikikautsarfahmist@gmail.com>
2025-11-07 14:36:16 +0700

    feat: add environment variable using Viper, make func load connfig and setup on main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/dwikikf/agviano-core-api-golang/internal/delivery/http"
+import (
+	"fmt"
+	"log"
+
+	"github.com/dwikikf/agviano-core-api-golang/internal/config"
+	"github.com/dwikikf/agviano-core-api-golang/internal/delivery/http"
+)
 
 func main() {
 	// Application entry point
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("❌ Gagal memuat konfigurasi: %v", err)
+	}
+
+	fmt.Printf("🚀 Memulai %s di lingkungan %s pada port %s, %s\n", config.AppName, config.AppEnv, config.AppPort, config.Message)
+
 	router := http.NewRouter()
-	router.Run() // listen and serve on
+	router.Run(":" + config.AppPort)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AppName  string `mapstructure:"APP_NAME"`
+	AppEnv   string `mapstructure:"APP_ENV"`
+	AppPort  string `mapstructure:"APP_PORT"`
+	Database DatabaseConfig
+	JWT      JWTConfig
+	LogLevel string `mapstructure:"LOG_LEVEL"`
+	Message  string `mapstructure:"MESSAGE"`
+}
+
+type DatabaseConfig struct {
+	Host     string `mapstructure:"DB_HOST"`
+	Port     string `mapstructure:"DB_PORT"`
+	User     string `mapstructure:"DB_USER"`
+	Password string `mapstructure:"DB_PASSWORD"`
+	Name     string `mapstructure:"DB_NAME"`
+}
+
+type JWTConfig struct {
+	SecretKey  string `mapstructure:"JWT_SECRET_KEY"`
+	ExpireHour int    `mapstructure:"JWT_EXPIRE_HOUR"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.SetDefault("APP_NAME", "Agviano Core API")
+	viper.SetDefault("APP_ENV", "Depelopment")
+	viper.SetDefault("APP_PORT", "8080")
+	viper.SetDefault("MESSAGE", "Default")
+
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(filepath.Join(path, ".env"))
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Println("⚠️ .env file not found, relying on environment variables or defaults.")
+		} else {
+			return
+		}
+	} else {
+		fmt.Println("✅ .env file loaded successfully.")
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
